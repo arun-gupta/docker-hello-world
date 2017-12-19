bcd030a41d2ab6165a9897e68bcfbd9d1010fdb2
Arun Gupta <arun.gupta@gmail.com>
2017-12-18 22:11:21 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    var greeting = "hello world!"
+    res.send(greeting);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
